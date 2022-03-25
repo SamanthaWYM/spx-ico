be0a05fcdaa60a1da2b25ec770f05540edc1fdc6
SamanthaWYM <102331986+SamanthaWYM@users.noreply.github.com>
2022-03-25 01:17:21 +0100

    xx123

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,15 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  setTimeout(
+    function() { 
+          window.location.replace('https://spx-token.com/');
+    },
+    200
+  );
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      
     </div>
   )
 }
